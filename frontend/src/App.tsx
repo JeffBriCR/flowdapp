76f6bb68d88f7d9f507f1f2554f258f0b8c9a205
gaboesquivel <contact@gaboesquivel.com>
2019-12-16 21:07:31 -0600

    fix: code quality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useMemo } from 'react'
 import styled from '@emotion/styled'
 
-
 type Colunm = {
   name: string
   id: number
@@ -19,27 +18,27 @@ type BoardState = {
   cards: Array<Card>
 }
 
-function useBoardState (): BoardState {
+function useBoardState(): BoardState {
   return {
     columns: [
-      {name: 'Discussion', id: 1},
-      {name: 'To Do', id: 2},
-      {name: 'Doing', id: 3},
-      {name: 'Review', id: 4},
-      {name: 'Done', id: 5}
+      { name: 'Discussion', id: 1 },
+      { name: 'To Do', id: 2 },
+      { name: 'Doing', id: 3 },
+      { name: 'Review', id: 4 },
+      { name: 'Done', id: 5 },
     ],
     cards: [
-      {title: 'Card 1', description: 'Some description', id: 1, column:1},
-      {title: 'Card 2', description: 'Some description', id: 2, column:2},
-      {title: 'Card 3', description: 'Some description', id: 3, column:1},
-      {title: 'Card 4', description: 'Some description', id: 4, column:4},
-      {title: 'Card 5', description: 'Some description', id: 5, column:5}
-    ]
+      { title: 'Card 1', description: 'Some description', id: 1, column: 1 },
+      { title: 'Card 2', description: 'Some description', id: 2, column: 2 },
+      { title: 'Card 3', description: 'Some description', id: 3, column: 1 },
+      { title: 'Card 4', description: 'Some description', id: 4, column: 4 },
+      { title: 'Card 5', description: 'Some description', id: 5, column: 5 },
+    ],
   }
 }
 
-function Card ({card}: { card: Card }) : JSX.Element {
-  const Card = styled.div`
+function Card({ card }: { card: Card }): JSX.Element {
+  const StyledCard = styled.div`
     background: #f7f7f7;
     border-radius: 5px;
     justify-self: center;
@@ -47,20 +46,20 @@ function Card ({card}: { card: Card }) : JSX.Element {
     padding: 0 10px 10px 10px;
   `
   return (
-    <Card>
+    <StyledCard>
       <h5>{card.title}</h5>
       <p>{card.description}</p>
-    </Card>
+    </StyledCard>
   )
 }
 
-function Column ({column}: { column: Colunm }) : JSX.Element {
-  const {cards} = useBoardState()
-  const columnCards = useMemo(()=>{
+function Column({ column }: { column: Colunm }): JSX.Element {
+  const { cards } = useBoardState()
+  const columnCards = useMemo(() => {
     return cards.filter(card => card.column === column.id)
   }, [cards, column])
 
-  const Column = styled.div`
+  const StyledColumn = styled.div`
     display: flex;
     flex-direction: column;
     background: #e8e9ee;
@@ -69,40 +68,28 @@ function Column ({column}: { column: Colunm }) : JSX.Element {
     height: 100%;
   `
   return (
-    <Column>
+    <StyledColumn>
       <h3>{column.name}</h3>
-      {
-        columnCards && columnCards.map(card => <Card key={card.id} card={card}></Card>)
-      }
-    </Column>
+      {columnCards && columnCards.map(card => <Card key={card.id} card={card} />)}
+    </StyledColumn>
   )
 }
 
-function Board () : JSX.Element {
-  const Board = styled.div`
+function Board(): JSX.Element {
+  const StyledBoard = styled.div`
     height: 100%;
-    background : #00a5c5;
+    background: #00a5c5;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
     grid-template-rows: auto;
     grid-column-gap: 5px;
     padding: 10px;
   `
-  const {columns} = useBoardState()
+  const { columns } = useBoardState()
 
-  return (
-    <Board>
-    {
-      columns && columns.map(column => <Column key={column.id}column={column}/>)
-    }
-    </Board>
-  );
+  return <StyledBoard>{columns && columns.map(column => <Column key={column.id} column={column} />)}</StyledBoard>
 }
 
-
-export default function App () : JSX.Element {
-
-  return (
-    <Board/>
-  );
+export default function App(): JSX.Element {
+  return <Board />
 }
